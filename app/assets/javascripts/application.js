190cd70ad394f0fd1da70ceee74062ebf6419a49
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-01 19:33:44 -0300

    Massaging adsense code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,15 @@ $(document).ready(function(){
     message: $('.js-cookie-message').html(),
     acceptText: 'OK'
   });
+
+  // Enable page level ads
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5360961269901609",
+    enable_page_level_ads: true
+  });
+
+  // Load adsense units after DOM is ready and styles applied
+  $('.adsbygoogle').each(function(){
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  });
 });
